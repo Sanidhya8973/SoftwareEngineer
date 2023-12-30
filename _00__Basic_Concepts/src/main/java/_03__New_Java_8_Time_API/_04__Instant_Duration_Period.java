046ba6de92121677a72c0ac88b9d8936739b20a6
Sanidhya8973 <sankokothari@gmail.com>
2023-12-31 05:06:25 +0530

    Learning Java 8 Feature: package java.time.*;

---
+++ _00__Basic_Concepts/src/main/java/_03__New_Java_8_Time_API/_04__Instant_Duration_Period.java
@@ -0,0 +1,98 @@
+package _03__New_Java_8_Time_API;
+
+import org.apache.log4j.*;
+
+import java.time.*;
+
+public class _04__Instant_Duration_Period {
+
+    public static final Logger log = LogManager.getLogger(_04__Instant_Duration_Period.class);
+
+    public static void initialiseLog() {
+        Layout layout = new PatternLayout("%n> LOG-ENTRY:- %n  -> [ %d ] %n     -> [ %C.%M ] %n        -> %p - %m %n");
+        Appender appender = new ConsoleAppender(layout);
+        log.addAppender(appender);
+    }
+
+    public static void main(String[] args) throws Exception {
+        initialiseLog();
+        // 1. Instant class with Duration class
+        Instant instant1 = Instant.ofEpochSecond(5);
+        Instant instant2 = Instant.ofEpochSecond(10, 1_000);
+        Instant instant3 = Instant.ofEpochSecond(10, -1_000);
+        System.out.println("> " + instant1 + "\n" + "> " + instant2 + "\n" + "> " + instant3);
+        Instant startInstant = Instant.now();
+        Thread.sleep(1000);
+        Instant endInstant = Instant.now();
+        Duration durationInstant = Duration.between(startInstant, endInstant);
+        // 2. LocalTime class with Duration class
+        LocalTime startLocalTime = LocalTime.of(06, 30, 10);
+        LocalTime endLocalTime = LocalTime.of(20, 30, 20);
+        Duration durationLocalTime = Duration.between(startLocalTime, endLocalTime);
+        // 3. LocalDateTime class with Duration class
+        LocalDateTime startDateTime = LocalDateTime.of(2023, 12, 29, 06, 30, 10);
+        LocalDateTime endDateTime = LocalDateTime.of(2023, 12, 30, 03, 30, 20);
+        Duration durationDateTime = Duration.between(startDateTime, endDateTime);
+        // 4. LocalDate class with Period class
+        LocalDate startLocalDate = LocalDate.of(2003, 3, 4);
+        LocalDate endLocalDate = LocalDate.now();
+        Period periodLocalDate = Period.between(startLocalDate, endLocalDate);
+        // logging all date and time related data
+        executeLog(durationInstant);
+        executeLog(durationLocalTime);
+        executeLog(durationDateTime);
+        executeLog(periodLocalDate);
+    }
+
+    public static void executeLog(Period period) {
+        log.info("duration             : " + period
+                + "\n                - duration.hashcode    : " + period.hashCode()
+                + "\n                - duration.isNegative(): " + period.isNegative()
+                + "\n                - duration.isZero()    : " + period.isZero()
+                + "\n                - duration.getUnits()  : " + period.getUnits()
+                + "\n                - duration.getClass()  : " + period.getClass()
+                + "\n                  - duration. : " + period
+        );
+    }
+
+    public static void executeLog(Duration duration) {
+        log.info("duration             : " + duration
+                + "\n                - duration.hashcode    : " + duration.hashCode()
+                + "\n                - duration.isNegative(): " + duration.isNegative()
+                + "\n                - duration.isPositive(): " + duration.isPositive()
+                + "\n                - duration.isZero()    : " + duration.isZero()
+                + "\n                  - duration.getNano()       : " + duration.getNano()
+                + "\n                  - duration.getSeconds()    : " + duration.getSeconds()
+                + "\n                  - duration.getUnits()      : " + duration.getUnits()
+                + "\n                  - duration.getClass()      : " + duration.getClass()
+                + "\n                  - duration.toNanos()       : " + duration.toNanos()
+                + "\n                  - duration.toMillis()      : " + duration.toMillis()
+                + "\n                  - duration.toSeconds()     : " + duration.toSeconds()
+                + "\n                  - duration.toMinutes()     : " + duration.toMinutes()
+                + "\n                  - duration.toHours()       : " + duration.toHours()
+                + "\n                  - duration.toDays()        : " + duration.toDays()
+                + "\n                  - duration.toNanosPart()   : " + duration.toNanosPart()
+                + "\n                  - duration.toMillisPart()  : " + duration.toMillisPart()
+                + "\n                  - duration.toSecondsPart() : " + duration.toSecondsPart()
+                + "\n                  - duration.toMinutesPart() : " + duration.toMinutesPart()
+                + "\n                  - duration.toHoursPart()   : " + duration.toHoursPart()
+                + "\n                  - duration.toDaysPart()    : " + duration.toDaysPart()
+                + "\n                  - duration.abs()                : " + duration.abs()
+                + "\n                  - duration.abs().hashCode()     : " + duration.abs().hashCode()
+                + "\n                  - duration.abs().toString()     : " + duration.abs().toString()
+                + "\n                  - duration.abs().toNanos()      : " + duration.abs().toNanos()
+                + "\n                  - duration.abs().toMillis()     : " + duration.abs().toMillis()
+                + "\n                  - duration.abs().toSeconds()    : " + duration.abs().toSeconds()
+                + "\n                  - duration.abs().toMinutes()    : " + duration.abs().toMinutes()
+                + "\n                  - duration.abs().toHours()      : " + duration.abs().toHours()
+                + "\n                  - duration.abs().toDays()       : " + duration.abs().toDays()
+                + "\n                  - duration.abs().toNanosPart()  : " + duration.abs().toNanosPart()
+                + "\n                  - duration.abs().toMillisPart() : " + duration.abs().toMillisPart()
+                + "\n                  - duration.abs().toSecondsPart(): " + duration.abs().toSecondsPart()
+                + "\n                  - duration.abs().toMinutesPart(): " + duration.abs().toMinutesPart()
+                + "\n                  - duration.abs().toHoursPart()  : " + duration.abs().toHoursPart()
+                + "\n                  - duration.abs().toDaysPart()   : " + duration.abs().toDaysPart()
+        );
+    }
+
+}
---
+++ _00__Basic_Concepts/src/main/java/_03__New_Java_8_Time_API/_05__ZoneId_ZoneOffset_ZonedDateTime_OffsetDateTime.java
@@ -0,0 +1,65 @@
+package _03__New_Java_8_Time_API;
+
+import org.apache.log4j.*;
+
+import java.time.*;
+import java.time.chrono.*;
+
+public class _05__ZoneId_ZoneOffset_ZonedDateTime_OffsetDateTime {
+
+    private static final Logger log = LogManager.getLogger(_05__ZoneId_ZoneOffset_ZonedDateTime_OffsetDateTime.class);
+
+    public static void initialiseLog() {
+        Layout layout = new PatternLayout("%n> LOG-ENTRY:- %n  -> [ %d ] %n     -> [ %C.%M ] %n        -> %p - %m %n");
+        Appender appender = new ConsoleAppender(layout);
+        log.addAppender(appender);
+    }
+
+    public static void main(String[] args) {
+        initialiseLog();
+        // 1. ZoneId class with ZonedDateTime, OffsetDateTime class
+        ZoneId myZoneId = ZoneId.of("Asia/Kolkata");
+        ZonedDateTime myZonedDateTimeByZoneId = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        ZonedDateTime myZonedDateTimeCompareByZoneId = myZonedDateTimeByZoneId.withZoneSameInstant(ZoneId.of("America/New_York"));
+        ZonedDateTime myZonedDateTimeCompareByZoneOffset = myZonedDateTimeByZoneId.withZoneSameInstant(ZoneOffset.of("-05:00"));
+        OffsetDateTime myOffsetDateTimeByZoneId = OffsetDateTime.now(ZoneId.of("Asia/Kolkata"));
+        // 2. ZoneOffset class with ZonedDateTime, OffsetDateTime class
+        // use OffsetDateTime to deal with database operations related to storing dates and time
+        // best way for database operations
+        ZoneOffset myZoneOffset = ZoneOffset.of("+05:30");
+        ZonedDateTime myZonedDateTimeByZoneOffset = ZonedDateTime.now(ZoneOffset.of("+05:30"));
+        OffsetDateTime myOffsetDateTimeByZoneOffset = OffsetDateTime.now(ZoneOffset.of("+05:30"));
+        OffsetDateTime myOffsetDateTimeCompare = myOffsetDateTimeByZoneOffset.withOffsetSameInstant(ZoneOffset.of("-05:00"));
+        // best way to deal with custom dates stored in database that should be reflected to other countries according to ZoneOffset
+        OffsetDateTime birthTimeAllCountries = OffsetDateTime.of(LocalDateTime.of(2003, 3, 4, 12, 30, 10), ZoneOffset.of("+05:30"));
+        System.out.println("> BirthDay India: " + birthTimeAllCountries);
+        birthTimeAllCountries = OffsetDateTime.of(LocalDateTime.of(2003, 3, 4, 12, 30, 10), ZoneOffset.of("+01:00"));
+        System.out.println("> BirthDay Paris: " + birthTimeAllCountries);
+        // few interesting methods
+        System.out.println("> HijrahDate now: " + HijrahDate.now());
+        // seconds passed from Jan 1st, 1970 till now
+        System.out.println("> OffsetDateTime: " + myOffsetDateTimeByZoneOffset.toEpochSecond());
+        System.out.println("> ZoneDateTime  : " + myZonedDateTimeCompareByZoneOffset.toEpochSecond());
+        // custom function for logging process
+        executeLog(myZoneId, myZonedDateTimeByZoneId, myZonedDateTimeCompareByZoneId, myZonedDateTimeCompareByZoneOffset, myOffsetDateTimeByZoneId);
+        executeLog(myZoneOffset, myZonedDateTimeByZoneOffset, myOffsetDateTimeByZoneOffset, myOffsetDateTimeCompare);
+    }
+
+    public static void executeLog(ZoneId myZoneId, ZonedDateTime myZonedDateTimeByZoneId, ZonedDateTime myZonedDateTimeCompareByZoneId, ZonedDateTime myZonedDateTimeCompareByZoneOffset, OffsetDateTime myOffsetDateTimeByZoneId) {
+        log.info("ZoneId                            : " + myZoneId
+                + "\n                - ZonedDateTime(ByZoneId)           : " + myZonedDateTimeByZoneId
+                + "\n                - ZonedDateTimeCompare(ByZoneId)    : " + myZonedDateTimeCompareByZoneId
+                + "\n                - ZonedDateTimeCompare(ByZoneOffset): " + myZonedDateTimeCompareByZoneOffset
+                + "\n                - OffsetDateTime(ByZoneId)          : " + myOffsetDateTimeByZoneId
+        );
+    }
+
+    public static void executeLog(ZoneOffset myZoneOffset, ZonedDateTime myZonedDateTimeByZoneOffset, OffsetDateTime myOffsetDateTimeByZoneOffset, OffsetDateTime myOffsetDateTimeCompare) {
+        log.info("ZoneOffset                  : " + myZoneOffset
+                + "\n                - ZonedDateTime(ByZoneOffset) : " + myZonedDateTimeByZoneOffset
+                + "\n                - OffsetDateTime(ByZoneOffset): " + myOffsetDateTimeByZoneOffset
+                + "\n                - OffsetDateTimeCompare       : " + myOffsetDateTimeCompare
+        );
+    }
+
+}
