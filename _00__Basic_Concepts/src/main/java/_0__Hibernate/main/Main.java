93756105d55376b7137d9e72f9e9cd3166590b01
Sanidhya8973 <sankokothari@gmail.com>
2024-01-28 12:16:42 +0530

    3 Ways To Create A SessionFactory Object In Hibernate

---
+++ _00__Basic_Concepts/src/main/java/_0__Hibernate/main/Main.java
@@ -0,0 +1,28 @@
+package _0__Hibernate.main;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import _0__Hibernate.config.HibernateJavaConfig;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Method - 1: xml based configurations
+        Configuration cfg = new Configuration();
+        cfg.configure("/com/company/project/config/hibernate.cfg.xml");
+        SessionFactory sessionFactory1 = cfg.buildSessionFactory();
+        System.out.println(sessionFactory1);
+
+        // Method - 2: xml based configurations
+        var sessionFactory2 = new Configuration().configure("/com/company/project/config/hibernate.cfg.xml").buildSessionFactory();
+        System.out.println(sessionFactory2);
+
+        // Method - 3: java based configurations
+        SessionFactory sessionFactory3 = HibernateJavaConfig.getSessionFactory();
+        System.out.println(sessionFactory3);
+
+    }
+
+}
