180aacc68bbc1aa6091bc862087510b0a42fe5b9
Sanidhya8973 <sankokothari@gmail.com>
2024-01-31 21:18:48 +0530

    Hibernate Java Based Configuration

--- _00__Basic_Concepts/src/main/java/_0__Hibernate/_01__config/HibernateConfig.java
+++ _00__Basic_Concepts/src/main/java/_0__Hibernate/_01__config/HibernateConfig.java
@@ -1,14 +1,18 @@
-package _0__Hibernate.config;
+package _0__Hibernate._01__config;
+
+import _0__Hibernate._04__company.model.Address;
+import _0__Hibernate._04__company.model.Certificate;
+import _0__Hibernate._04__company.model.Employee;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import java.util.Properties;
 
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
-public class HibernateJavaConfig {
+public class HibernateConfig {
 
     private static SessionFactory sessionFactory;
 
@@ -17,14 +21,20 @@ public class HibernateJavaConfig {
             Configuration cfg = new Configuration();
             Properties property = new Properties();
             property.put(Environment.JAKARTA_JDBC_DRIVER, "com.mysql.cj.jdbc.Driver");
-            property.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://127.0.0.1:3306/school?useSSL=false");
+            property.put(Environment.JAKARTA_JDBC_URL, "jdbc:mysql://127.0.0.1:3306/hibernate_db?useSSL=false");
             property.put(Environment.JAKARTA_JDBC_USER, "root");
             property.put(Environment.JAKARTA_JDBC_PASSWORD, "root");
-            property.put(Environment.HBM2DDL_AUTO, "update");
-            property.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+            property.put(Environment.HBM2DDL_AUTO, "create");
+            //property.put(Environment.HBM2DDL_AUTO, "update");
+            property.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
             property.put(Environment.SHOW_SQL, true);
             property.put(Environment.FORMAT_SQL, true);
             cfg.setProperties(property);
+
+            cfg.addAnnotatedClass(Employee.class);
+            cfg.addAnnotatedClass(Certificate.class);
+            cfg.addAnnotatedClass(Address.class);
+
             ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(cfg.getProperties()).build();
             sessionFactory = cfg.buildSessionFactory(serviceRegistry);
         }
