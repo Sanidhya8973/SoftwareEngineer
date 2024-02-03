0b7fba0a7ec8c8143f495c2b3dbf4bf0d2b1a8a6
Sanidhya8973 <sankokothari@gmail.com>
2024-02-03 23:14:10 +0530

    Java 8: Interface Example -> default Keyword

--- _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Default_Method/B.java
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Default_Method/B.java
@@ -2,8 +2,8 @@ package _03__Java_8.Java_Interface.Example_Default_Method;
 
 public interface B {
 
-    public default String method1() {
-        return "> Interface B: default method";
+    public default void method1() {
+        System.out.println("> Interface B: default method");
     }
 
     public abstract void method2();
--- _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Default_Method/C.java
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Default_Method/C.java
@@ -2,16 +2,23 @@ package _03__Java_8.Java_Interface.Example_Default_Method;
 
 public class C implements A, B {
 
-    public String method1() {
+    @Override
+    public void method1() {
         // we have to choose what we have to return from 3 different things
         // return "> Interface C: overrides default method of A and B";
         // return A.super.method1();
         // return B.super.method1();
-        return "Interface C: overrides default method of A and B";
+        System.out.println("> Interface C: overrides default method of A and B");
     }
 
+    @Override
     public void method2() {
         // business logic
+    }
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.method1();
     }
 
 }
