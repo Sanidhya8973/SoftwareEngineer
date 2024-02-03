246190dfe138b6ccad743d270a2896ae98918523
Sanidhya8973 <sankokothari@gmail.com>
2024-02-03 23:04:34 +0530

    Java 8: Interface Example

---
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Static_Method/A.java
@@ -0,0 +1,13 @@
+package _03__Java_8.Java_Interface.Example_Static_Method;
+
+public interface A {
+
+    public static void method() {
+        System.out.println("> Interface A: static method");
+    }
+
+    public static void main(String[] args) {
+        // since java 8, static methods are allowed, so we can write a main method inside an interface and execute it as well
+    }
+
+}
---
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/Example_Static_Method/B.java
@@ -0,0 +1,21 @@
+package _03__Java_8.Java_Interface.Example_Static_Method;
+
+public class B implements A {
+
+    // here, method() of interface A is not overridden
+    // static methods have no concept of overridden in the implementing classes
+    private static void method() {
+        System.out.println("> Interface B: static  method");
+    }
+
+    public static void main(String[] args) {
+        // method - 1
+        B b = new B();
+        b.method();
+        // method - 2
+        B.method();
+        // we can call the static method from interface A
+        A.method();
+    }
+
+}
