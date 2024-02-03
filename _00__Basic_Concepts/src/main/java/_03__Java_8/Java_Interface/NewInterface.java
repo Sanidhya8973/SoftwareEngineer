ca66c519a97aab3e6c21738e65e5e34aac810c4f
Sanidhya8973 <sankokothari@gmail.com>
2024-02-03 23:05:52 +0530

    Java 8: Interface Updates

--- _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/NewInterface.java
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Interface/NewInterface.java
@@ -13,6 +13,10 @@ public interface NewInterface {
         // static business logic
     }
 
+    public static void main(String[] args) {
+        // since java 8, static methods are allowed, so we can write a main method inside an interface and execute it as well
+    }
+
     // method - 2
     void method4();
 
