36b93e71e1ed833fcd3145b771d2dab8259a75d4
Sanidhya8973 <sankokothari@gmail.com>
2024-02-04 19:03:16 +0530

    Java 8: Lambda Expression: @FunctionalInterface - with return type and parameters

---
+++ _00__Basic_Concepts/src/main/java/_03__Java_8/Java_Lambda_Expression/LambdaExpression4.java
@@ -0,0 +1,8 @@
+package _03__Java_8.Java_Lambda_Expression;
+
+public interface LambdaExpression4 {
+
+    // only 1 abstract method is allowed and N number of static and default methods
+    public abstract int magic(String str, int a, int b);
+
+}
