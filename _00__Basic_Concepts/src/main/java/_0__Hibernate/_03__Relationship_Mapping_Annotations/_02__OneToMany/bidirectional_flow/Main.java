40f48ddae05e399f82fb51e3f58119a072f71dc5
Sanidhya8973 <sankokothari@gmail.com>
2024-02-12 00:07:22 +0530

    Hibernate Configuration: @OneToMany annotation: bi-directional flow

--- _00__Basic_Concepts/src/main/java/_0__Hibernate/_03__Relationship_Mapping_Annotations/_02__OneToMany/bidirectional_flow/Main.java
+++ _00__Basic_Concepts/src/main/java/_0__Hibernate/_03__Relationship_Mapping_Annotations/_02__OneToMany/bidirectional_flow/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         testOneToManyAndManyToOne();
+        // testFlow()
     }
 
     public static void testOneToManyAndManyToOne() {
