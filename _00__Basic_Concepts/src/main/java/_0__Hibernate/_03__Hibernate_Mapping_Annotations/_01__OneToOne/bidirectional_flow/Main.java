f7c3678f7cb59cb55ed3225b93b43887d2a27d01
Sanidhya8973 <sankokothari@gmail.com>
2024-02-13 21:45:53 +0530

    Hibernate: Mapping Annotations: OneToOne Bi-Directional Flow

--- _00__Basic_Concepts/src/main/java/_0__Hibernate/_03__Hibernate_Mapping_Annotations/_01__OneToOne/bidirectional_flow/Main.java
+++ _00__Basic_Concepts/src/main/java/_0__Hibernate/_03__Hibernate_Mapping_Annotations/_01__OneToOne/bidirectional_flow/Main.java
@@ -1,12 +1,11 @@
-package _0__Hibernate._03__Relationship_Mapping_Annotations._01__OneToOne.bidirectional_flow;
+package _0__Hibernate._03__Hibernate_Mapping_Annotations._01__OneToOne.bidirectional_flow;
 
-import _0__Hibernate._03__Relationship_Mapping_Annotations._01__OneToOne.bidirectional_flow.config.HibernateConfig;
-import _0__Hibernate._03__Relationship_Mapping_Annotations._01__OneToOne.bidirectional_flow.model.Answer;
-import _0__Hibernate._03__Relationship_Mapping_Annotations._01__OneToOne.bidirectional_flow.model.Question;
+import _0__Hibernate._03__Hibernate_Mapping_Annotations._01__OneToOne.bidirectional_flow.config.HibernateConfig;
+import _0__Hibernate._03__Hibernate_Mapping_Annotations._01__OneToOne.bidirectional_flow.model.Answer;
+import _0__Hibernate._03__Hibernate_Mapping_Annotations._01__OneToOne.bidirectional_flow.model.Question;
 import org.hibernate.*;
 
 import java.util.*;
-import java.util.function.*;
 
 public class Main {
 
