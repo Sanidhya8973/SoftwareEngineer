9edd475cb0bbfe7bc45f833907f3404039932215
Sanidhya8973 <sankokothari@gmail.com>
2024-02-15 23:51:35 +0530

    JUnit5: Method To Be Tested

--- _00__Basic_Concepts/src/main/java/_04__Lombok/Main.java
+++ _00__Basic_Concepts/src/main/java/_04__Lombok/Main.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class Main {
 
     public static void main(String[] args) {
-        TestUserA tua = new TestUserA();
+//        TestUserA tua = new TestUserA();
 //        tua.setId(1);
 //        tua.setName("user");
 //        tua.setEmail("user@mail.com");
---
+++ _00__Basic_Concepts/src/main/java/_06__JUnit/Main.java
@@ -0,0 +1,21 @@
+package _06__JUnit;
+
+public class Main {
+
+    public String reverse(String str) {
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+
+}
