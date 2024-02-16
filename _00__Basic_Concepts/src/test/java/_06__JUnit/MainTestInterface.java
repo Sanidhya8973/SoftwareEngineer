f1ed6e5fb8d3c68f3fae167e1f0ec589c09c0366
Sanidhya8973 <sankokothari@gmail.com>
2024-02-16 23:50:34 +0530

    Learning JUnit5

--- _00__Basic_Concepts/src/test/java/_06__JUnit/MainTestInterface.java
+++ _00__Basic_Concepts/src/test/java/_06__JUnit/MainTestInterface.java
@@ -3,6 +3,7 @@ package _06__JUnit;
 import org.junit.jupiter.api.*;
 
 public interface MainTestInterface {
+
     @Test
     public default void test_0() {
         System.out.println("Visibility of @Test annotation method can be public, protected and default (only available in interface)");
