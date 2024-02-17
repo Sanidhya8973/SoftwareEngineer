bf711289720f55ba3ba46405bc89001bb662008b
Sanidhya8973 <sankokothari@gmail.com>
2024-02-17 22:46:01 +0530

    Hibernate Annotations: JPA Entity Lifecycle Annotations

--- _00__Basic_Concepts/src/main/java/_07__Hibernate/_02__Hibernate_Core_Annotations/model/AuditEntityListeners.java
+++ _00__Basic_Concepts/src/main/java/_07__Hibernate/_02__Hibernate_Core_Annotations/model/AuditEntityListeners.java
@@ -16,63 +16,63 @@ public class AuditEntityListeners {
     @PrePersist
     public void prePersist(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PrePersist Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PrePersist Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PostPersist
     public void postPersist(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PostPersist Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PostPersist Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PreUpdate
     public void preUpdate(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PreUpdate Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PreUpdate Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PostUpdate
     public void postUpdate(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PostUpdate Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PostUpdate Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PreRemove
     public void preRemove(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PreRemove Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PreRemove Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PostRemove
     public void postRemove(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PostRemove Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PostRemove Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
     @PostLoad
     public void postLoad(Employee employee) {
         if (employee.getId() != 0) {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : @PostLoad Phase");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : @PostLoad Phase");
         } else {
-            System.out.println("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
+            log.info("> EMPLOYEE -> id=" + employee.getId() + " : NOT FOUND...");
         }
     }
 
