ec4e8efd6d004db164bb03c22eb682314ae3655a
Sanidhya8973 <sankokothari@gmail.com>
2024-02-23 21:51:34 +0530

    Spring Data JPA: Service Layer

--- _05__Spring_Data_JPA/src/main/java/com/company/project/service/ProductCategoryService.java
+++ _05__Spring_Data_JPA/src/main/java/com/company/project/service/ProductCategoryService.java
@@ -14,80 +14,80 @@ import com.company.project.repository.ProductCategoryRepository;
 
 public class ProductCategoryService {
 
-    @Autowired
-    private ProductCategoryRepository productCategoryRepository;
+	@Autowired
+	private ProductCategoryRepository productCategoryRepository;
 
-    public long count() {
-        return productCategoryRepository.count();
-    }
+	public long count() {
+		return productCategoryRepository.count();
+	}
 
-    public boolean existsById(Long id) {
-        return productCategoryRepository.existsById(id);
-    }
+	public boolean existsById(Long id) {
+		return productCategoryRepository.existsById(id);
+	}
 
-    public ProductCategory findById(Long id) {
-        return productCategoryRepository.findById(id).get();
-    }
+	public ProductCategory findById(Long id) {
+		return productCategoryRepository.findById(id).get();
+	}
 
-    public List<ProductCategory> findAll() {
-        return productCategoryRepository.findAll();
-    }
+	public List<ProductCategory> findAll() {
+		return productCategoryRepository.findAll();
+	}
 
-    public List<ProductCategory> findAllById(List<Long> productCategoryIdList) {
-        return productCategoryRepository.findAllById(productCategoryIdList);
-    }
+	public List<ProductCategory> findAllById(List<Long> productCategoryIdList) {
+		return productCategoryRepository.findAllById(productCategoryIdList);
+	}
 
-    public void save(ProductCategory productCategory) {
-        productCategoryRepository.save(productCategory);
-    }
+	public void save(ProductCategory productCategory) {
+		productCategoryRepository.save(productCategory);
+	}
 
-    public void saveAll(List<ProductCategory> productCategoryList) {
-        productCategoryRepository.saveAll(productCategoryList);
-    }
+	public void saveAll(List<ProductCategory> productCategoryList) {
+		productCategoryRepository.saveAll(productCategoryList);
+	}
 
-    public void update(Long id, ProductCategory newProductCategory) {
-        ProductCategory oldProductCategory = productCategoryRepository.findById(id).get();
-        if (oldProductCategory.getId() == newProductCategory.getId()) {
-            productCategoryRepository.save(newProductCategory);
-        }
-    }
+	public void update(Long id, ProductCategory newProductCategory) {
+		ProductCategory oldProductCategory = productCategoryRepository.findById(id).get();
+		if (oldProductCategory.getId() == newProductCategory.getId()) {
+			productCategoryRepository.save(newProductCategory);
+		}
+	}
 
-    public void deleteById(Long id) {
-        productCategoryRepository.deleteById(id);
-    }
+	public void deleteById(Long id) {
+		productCategoryRepository.deleteById(id);
+	}
 
-    public void delete(ProductCategory productCategory) {
-        productCategoryRepository.delete(productCategory);
-    }
+	public void delete(ProductCategory productCategory) {
+		productCategoryRepository.delete(productCategory);
+	}
 
-    public void deleteAll() {
-        productCategoryRepository.deleteAll();
-    }
+	public void deleteAll() {
+		productCategoryRepository.deleteAll();
+	}
 
-    public void deleteAll(List<ProductCategory> productCategoryList) {
-        productCategoryRepository.deleteAll(productCategoryList);
-    }
+	public void deleteAll(List<ProductCategory> productCategoryList) {
+		productCategoryRepository.deleteAll(productCategoryList);
+	}
 
-    public void deleteAllById(List<Long> productCategoryIdList) {
-        productCategoryRepository.deleteAllById(productCategoryIdList);
-    }
+	public void deleteAllById(List<Long> productCategoryIdList) {
+		productCategoryRepository.deleteAllById(productCategoryIdList);
+	}
 
-    public List<ProductCategory> doPaginationAndSorting(int pageNumber, int pageSize, String[] sortBy, String sortDir) {
-        List<Order> orderList = new ArrayList<>();
-        for (int i = 0; i < sortBy.length; i++) {
-            Order order;
-            if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
-                order = new Order(Sort.Direction.ASC, sortBy[i]);
-            } else {
-                order = new Order(Sort.Direction.DESC, sortBy[i]);
-            }
-            orderList.add(order);
-        }
-        Sort sort = Sort.by(orderList);
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
-        Page<ProductCategory> page = productCategoryRepository.findAll(pageable);
-        List<ProductCategory> productCategoryList = page.getContent();
-        return productCategoryList;
-    }
+	public List<ProductCategory> doPaginationAndSorting(int pageNumber, int pageSize, String[] sortBy, String sortDir) {
+		List<Order> orderList = new ArrayList<>();
+		for (int i = 0; i < sortBy.length; i++) {
+			Order order;
+			if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
+				order = new Order(Sort.Direction.ASC, sortBy[i]);
+			} else {
+				order = new Order(Sort.Direction.DESC, sortBy[i]);
+			}
+			orderList.add(order);
+		}
+		Sort sort = Sort.by(orderList);
+		Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+		Page<ProductCategory> page = productCategoryRepository.findAll(pageable);
+		List<ProductCategory> productCategoryList = page.getContent();
+		return productCategoryList;
+	}
 
 }
--- _05__Spring_Data_JPA/src/main/java/com/company/project/service/ProductService.java
+++ _05__Spring_Data_JPA/src/main/java/com/company/project/service/ProductService.java
@@ -17,123 +17,123 @@ import java.util.List;
 @Service(value = "service_product")
 public class ProductService {
 
-    @Autowired
-    private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-    public long count() {
-        return productRepository.count();
-    }
+	public long count() {
+		return productRepository.count();
+	}
 
-    public boolean existsById(Long id) {
-        return productRepository.existsById(id);
-    }
+	public boolean existsById(Long id) {
+		return productRepository.existsById(id);
+	}
 
-    public Product findById(Long id) {
-        return productRepository.findById(id).get();
-    }
+	public Product findById(Long id) {
+		return productRepository.findById(id).get();
+	}
 
-    public List<Product> findAll() {
-        return productRepository.findAll();
-    }
+	public List<Product> findAll() {
+		return productRepository.findAll();
+	}
 
-    public List<Product> findAllById(List<Long> productIdList) {
-        return productRepository.findAllById(productIdList);
-    }
+	public List<Product> findAllById(List<Long> productIdList) {
+		return productRepository.findAllById(productIdList);
+	}
 
-    public void save(Product product) {
-        productRepository.save(product);
-    }
+	public void save(Product product) {
+		productRepository.save(product);
+	}
 
-    public void saveAll(List<Product> productList) {
-        productRepository.saveAll(productList);
-    }
+	public void saveAll(List<Product> productList) {
+		productRepository.saveAll(productList);
+	}
 
-    public void update(Long id, Product newProduct) {
-        Product oldProduct = productRepository.findById(id).get();
-        if (oldProduct.getId() == newProduct.getId()) {
-            productRepository.save(newProduct);
-        }
-    }
+	public void update(Long id, Product newProduct) {
+		Product oldProduct = productRepository.findById(id).get();
+		if (oldProduct.getId() == newProduct.getId()) {
+			productRepository.save(newProduct);
+		}
+	}
 
-    public void deleteById(Long id) {
-        productRepository.deleteById(id);
-    }
+	public void deleteById(Long id) {
+		productRepository.deleteById(id);
+	}
 
-    public void delete(Product product) {
-        productRepository.delete(product);
-    }
+	public void delete(Product product) {
+		productRepository.delete(product);
+	}
 
-    public void deleteAll() {
-        productRepository.deleteAll();
-    }
+	public void deleteAll() {
+		productRepository.deleteAll();
+	}
 
-    public void deleteAll(List<Product> productList) {
-        productRepository.deleteAll(productList);
-    }
+	public void deleteAll(List<Product> productList) {
+		productRepository.deleteAll(productList);
+	}
 
-    public void deleteAllById(List<Long> productIdList) {
-        productRepository.deleteAllById(productIdList);
-    }
+	public void deleteAllById(List<Long> productIdList) {
+		productRepository.deleteAllById(productIdList);
+	}
 
-    public List<Product> pagination(int pageNumber, int pageSize) {
-        Pageable pageable = PageRequest.of(pageNumber, pageSize);
-        Page<Product> page = productRepository.findAll(pageable);
-        List<Product> productList = page.getContent();
-        return productList;
-    }
+	public List<Product> pagination(int pageNumber, int pageSize) {
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<Product> page = productRepository.findAll(pageable);
+		List<Product> productList = page.getContent();
+		return productList;
+	}
 
-    public List<Product> sortingSingle(String sortBy, String sortDir) {
-        Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
-                : Sort.by(sortBy).descending();
-        List<Product> productList = productRepository.findAll(sort);
-        return productList;
-    }
+	public List<Product> sortingSingle(String sortBy, String sortDir) {
+		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+				: Sort.by(sortBy).descending();
+		List<Product> productList = productRepository.findAll(sort);
+		return productList;
+	}
 
-    public List<Product> sortingDouble(String sortByField1, String sortByField2, String sortDir) {
-        Sort sortField1 = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortByField1).ascending()
-                : Sort.by(sortByField1).descending();
-        Sort sortField2 = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortByField2).ascending()
-                : Sort.by(sortByField2).descending();
-        Sort groupSort = sortField1.and(sortField2);
-        List<Product> productList = productRepository.findAll(groupSort);
-        return productList;
-    }
+	public List<Product> sortingDouble(String sortByField1, String sortByField2, String sortDir) {
+		Sort sortField1 = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortByField1).ascending()
+				: Sort.by(sortByField1).descending();
+		Sort sortField2 = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortByField2).ascending()
+				: Sort.by(sortByField2).descending();
+		Sort groupSort = sortField1.and(sortField2);
+		List<Product> productList = productRepository.findAll(groupSort);
+		return productList;
+	}
 
-    public List<Product> sortingMultiple(String[] sortBy, String sortDir) {
-        List<Order> orderList = new ArrayList<>();
-        for (int i = 0; i < sortBy.length; i++) {
-            Order order;
-            if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
-                order = new Order(Sort.Direction.ASC, sortBy[i]);
-            } else {
-                order = new Order(Sort.Direction.DESC, sortBy[i]);
-            }
-            orderList.add(order);
-        }
+	public List<Product> sortingMultiple(String[] sortBy, String sortDir) {
+		List<Order> orderList = new ArrayList<>();
+		for (int i = 0; i < sortBy.length; i++) {
+			Order order;
+			if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
+				order = new Order(Sort.Direction.ASC, sortBy[i]);
+			} else {
+				order = new Order(Sort.Direction.DESC, sortBy[i]);
+			}
+			orderList.add(order);
+		}
 
-        Sort groupSort = Sort.by(orderList);
+		Sort groupSort = Sort.by(orderList);
 
-        List<Product> productList = productRepository.findAll(groupSort);
+		List<Product> productList = productRepository.findAll(groupSort);
 
-        return productList;
-    }
+		return productList;
+	}
 
-    public List<Product> doPaginationAndSorting(int pageNumber, int pageSize, String[] sortBy, String sortDir) {
-        List<Order> orderList = new ArrayList<>();
-        for (int i = 0; i < sortBy.length; i++) {
-            Order order;
-            if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
-                order = new Order(Sort.Direction.ASC, sortBy[i]);
-            } else {
-                order = new Order(Sort.Direction.DESC, sortBy[i]);
-            }
-            orderList.add(order);
-        }
-        Sort sort = Sort.by(orderList);
-        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
-        Page<Product> page = productRepository.findAll(pageable);
-        List<Product> productList = page.getContent();
-        return productList;
-    }
+	public List<Product> doPaginationAndSorting(int pageNumber, int pageSize, String[] sortBy, String sortDir) {
+		List<Order> orderList = new ArrayList<>();
+		for (int i = 0; i < sortBy.length; i++) {
+			Order order;
+			if (sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())) {
+				order = new Order(Sort.Direction.ASC, sortBy[i]);
+			} else {
+				order = new Order(Sort.Direction.DESC, sortBy[i]);
+			}
+			orderList.add(order);
+		}
+		Sort sort = Sort.by(orderList);
+		Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+		Page<Product> page = productRepository.findAll(pageable);
+		List<Product> productList = page.getContent();
+		return productList;
+	}
 
 }
