580af9e116c585653b5c6ba0fbb19e5fd50bbc56
Sanidhya8973 <sankokothari@gmail.com>
2024-03-01 18:48:44 +0530

    Spring Web MVC: Added ContactPrefix Class

---
+++ _08__Spring_Web_MVC/src/main/java/com/company/project/model/ContactPrefix.java
@@ -0,0 +1,38 @@
+package com.company.project.model;
+
+import lombok.*;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "entity_contact_prefix")
+@Table(name = "table_contact_prefix", schema = "ecommerce")
+public class ContactPrefix {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "contact_prefix_country_id")
+    private Long id;
+
+    @NotBlank(message = "please enter your country name")
+    @Size(min = 4, max = 43, message = "please enter a valid country name and length must be between {min} and {max}")
+    @Column(name = "contact_prefix_country_name", nullable = false, unique = true)
+    private String countryName;
+
+    @Pattern(regexp = "[+][0-9]+", message = "please provide a valid country code")
+    @Column(name = "contact_prefix_country_code", nullable = false, unique = true)
+    private String countryCode;
+
+    @Override
+    public String toString() {
+        return "ContactPrefix {" +
+                "id=" + id +
+                ", countryName='" + countryName + '\'' +
+                ", countryCode='" + countryCode + '\'' +
+                '}';
+    }
+
+}
