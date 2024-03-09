b38fdf2aef43de0563d7740ad8004360d59c61f8
Sanidhya8973 <sankokothari@gmail.com>
2024-03-09 16:02:49 +0530

    Spring Data JPA: Spring Boot Application

--- _06__Spring_Data_JPA/src/main/java/com/company/project/WebAppApplication.java
+++ _06__Spring_Data_JPA/src/main/java/com/company/project/WebAppApplication.java
@@ -2,12 +2,14 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class WebAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebAppApplication.class, args);
+    }
 
 }
