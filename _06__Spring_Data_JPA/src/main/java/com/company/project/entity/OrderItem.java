6e94d25577c8007254c46acca31308deddebfa11
Sanidhya8973 <sankokothari@gmail.com>
2024-03-09 16:02:26 +0530

    Spring Data JPA: OrderItem entity

--- _06__Spring_Data_JPA/src/main/java/com/company/project/entity/OrderItem.java
+++ _06__Spring_Data_JPA/src/main/java/com/company/project/entity/OrderItem.java
@@ -10,28 +10,28 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-@Entity(name = "entity_orders_item")
-@Table(name = "table_orders_item", schema = "ecommerce")
+@Entity(name = "entity_order_item")
+@Table(name = "table_order_item", schema = "ecommerce")
 public class OrderItem {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator_orders_item_id")
-	@SequenceGenerator(name = "generator_orders_item_id", sequenceName = "sequence_orders_item_id", initialValue = 1, allocationSize = 1)
-	@Column(name = "orders_item_id")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "generator_order_item_id")
+	@SequenceGenerator(name = "generator_order_item_id", sequenceName = "sequence_order_item_id", initialValue = 1, allocationSize = 1)
+	@Column(name = "order_item_id")
 	private Long id;
 
 	@ManyToOne
-	@JoinColumn(name = "fk_orders_id")
+	@JoinColumn(name = "fk_order_id")
 	private Order order;
 
 	@OneToOne
 	@JoinColumn(name = "fk_product_id", nullable = false)
 	private Product product;
 
-	@Column(name = "orders_item_quantity", nullable = false)
+	@Column(name = "order_item_quantity", nullable = false)
 	private int quantity;
 
-	@Column(name = "orders_item_product_price_per_quantity", nullable = false)
+	@Column(name = "order_item_product_price_per_quantity", nullable = false)
 	private BigDecimal totalOrdersItemAmount;
 
 	public BigDecimal getTotalAmount() {
