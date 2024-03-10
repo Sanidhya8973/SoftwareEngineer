101be0511850db9e3e18f36b72f576007b921b6e
Sanidhya8973 <sankokothari@gmail.com>
2024-03-10 20:17:19 +0530

    Spring Data JPA: Repository Layer

--- _06__Spring_Data_JPA/src/main/java/com/company/project/repository/UserRepository.java
+++ _06__Spring_Data_JPA/src/main/java/com/company/project/repository/UserRepository.java
@@ -1,12 +1,16 @@
 package com.company.project.repository;
 
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.company.project.entity.User;
 
 @Repository(value = "repository_user")
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = "SELECT * FROM table_user WHERE user_email = :email;", nativeQuery = true)
+    public abstract void readUserByEmail(@Param("email") String email) throws Exception;
 
 }
