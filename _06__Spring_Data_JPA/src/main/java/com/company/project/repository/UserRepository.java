414ffdaf63fbdb30605c1482c7d1b2cedcbdca2f
Sanidhya8973 <sankokothari@gmail.com>
2024-03-10 20:08:57 +0530

    Spring Data JPA: Testing Repository Layer

--- _06__Spring_Data_JPA/src/main/java/com/company/project/repository/UserRepository.java
+++ _06__Spring_Data_JPA/src/main/java/com/company/project/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.company.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.company.project.entity.User;
